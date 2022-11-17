ba6b5834dea1b7d806a285dc497d0c74f4bed75b
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2022-11-17 12:47:18 +0100

    Add input type number

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ export function registerDirectives(Component, router) {
     Component.directives['ui-href'] = (element, value, tag, attributes) => element.onclick = event => {
         const path = router.absolute(value, element.hostingComponent);
     
-        if (!router.getRoute(path)) {
+        if (!router.getRoute(path)) {
             if (attributes['ui-href-target'] == 'blank') {
                 open(path);
             } else {
@@ -48,7 +48,7 @@ export function registerDirectives(Component, router) {
     
     Component.directives['ui-href-active'] = (element, value, tag, attributes) => {
         function resolveActive() {
-            if (router.activePath.startsWith(router.absolute(value === true ? attributes['ui-href'] : value, element.hostingComponent))) {
+            if (router.activePath.startsWith(router.absolute(value === true ? attributes['ui-href'] : value, element.hostingComponent))) {
                 element.setAttribute('ui-active', '');
             } else {
                 element.removeAttribute('ui-active');
@@ -73,7 +73,7 @@ export function registerDirectives(Component, router) {
     Component.directives['ui-value'] = (element, value, tag) => {
         if (tag == 'option') {
             (element as any).dataValue = value;
-            element.value = Math.random().toString(16).substr(2);
+            element.value = Math.random().toString(16).substring(2);
     
             return;
         }
@@ -81,29 +81,29 @@ export function registerDirectives(Component, router) {
         throw 'use [$ui-value]'
     };
     
-    Component.directives['$ui-value'] = (element, accessor, tag, attriubtes, content) => {
+    Component.directives['$ui-value'] = (element, accessor, tag, attributes, content) => {
         if (tag == 'option') {
             throw 'use [ui-value]';
         }
     
-        if (attriubtes.type == 'checkbox') {
+        if (attributes.type == 'checkbox') {
             element.checked = accessor.get();
     
             element.onchange = () => {
                 accessor.set(element.checked);
     
-                attriubtes['ui-change'] && attriubtes['ui-change'](element.checked);
+                attributes['ui-change'] && attributes['ui-change'](element.checked);
             };
-        } else if (attriubtes.type == 'date') {
+        } else if (attributes.type == 'date') {
             element.type = 'date';
             element.valueAsDate = accessor.get();
     
             element.onchange = () => {
                 accessor.set(element.valueAsDate);
     
-                attriubtes['ui-change'] && attriubtes['ui-change'](element.valueAsDate);
+                attributes['ui-change'] && attributes['ui-change'](element.valueAsDate);
             };
-        } else if (attriubtes.type == 'datetime-local') {
+        } else if (attributes.type == 'datetime-local') {
             element.type = 'datetime-local';
             element.value = accessor.get()?.toISOString().replace('Z', '');
     
@@ -111,29 +111,37 @@ export function registerDirectives(Component, router) {
                 const value = new Date(element.value);
                 accessor.set(value);
     
-                attriubtes['ui-change'] && attriubtes['ui-change'](value);
+                attributes['ui-change'] && attributes['ui-change'](value);
             };
-        } else if (attriubtes.type == 'file') {
+        } else if (attributes.type == 'file') {
             element.type = 'file';
             element.files = accessor.get();
     
             element.onchange = () => {
                 accessor.set(element.files);
     
-                attriubtes['ui-change'] && attriubtes['ui-change'](element.files);
+                attributes['ui-change'] && attributes['ui-change'](element.files);
             };
         } else if (tag == 'select') {
             content = content.flat();
     
             const initialValue = accessor.get();
-            element.value = content.find(element => element.dataValue == initialValue || ((typeof element.dataValue == 'object') && (typeof initialValue == 'object') && ('id' in element.dataValue) && ('id' in initialValue) && element.dataValue?.id == initialValue?.id))?.value;
+            element.value = content.find(element => element.dataValue == initialValue || ((typeof element.dataValue == 'object') && (typeof initialValue == 'object') && ('id' in element.dataValue) && ('id' in initialValue) && element.dataValue?.id == initialValue?.id))?.value;
 
             element.onchange = () => {
                 const option = content.find(option => option.value == element.value);
     
                 accessor.set(option.dataValue);
     
-                attriubtes['ui-change'] && attriubtes['ui-change'](option.dataValue);
+                attributes['ui-change'] && attributes['ui-change'](option.dataValue);
+            };
+        } else if (attributes.type == 'number') {
+            element.value = accessor.get();
+            
+            element.onblur = () => {
+                accessor.set(+element.value);
+                
+                attributes['ui-change'] && attributes['ui-change'](+element.value);
             };
         } else {
             element.value = accessor.get();
@@ -141,7 +149,7 @@ export function registerDirectives(Component, router) {
             element.onblur = () => {
                 accessor.set(element.value);
                 
-                attriubtes['ui-change'] && attriubtes['ui-change'](element.value);
+                attributes['ui-change'] && attributes['ui-change'](element.value);
             };
         }
     };    
